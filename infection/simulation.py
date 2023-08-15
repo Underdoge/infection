31bf6fa88534bf9220192a876febac7915c5b231
Eduardo Chapa <eduardo.chapa@gmail.com>
2023-08-14 23:31:15 -0600

    max_workers=4

--- infection/simulation.py
+++ infection/simulation.py
@@ -53,7 +53,7 @@ class Simulation(App):
     def __init__(self, **kwargs):
         super(Simulation, self).__init__(**kwargs)
         self._thread_pool = ThreadPoolExecutor(
-            max_workers=200)
+            max_workers=4)
         self._threads = len(enumerate())
         self._quadtree = QuadTree((0, 0), Window.size[0], Window.size[1])
         self._population = []
@@ -210,7 +210,7 @@ class Simulation(App):
         self.menu.lbl_value_infected.text = "0"
         self.thread_pool.shutdown()
         self.thread_pool = ThreadPoolExecutor(
-            max_workers=200)
+            max_workers=4)
         del self.threads
         del self.population
         del self.healthy
